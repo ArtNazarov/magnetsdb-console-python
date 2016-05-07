c083d907bfdbe5af83a4134695a741bbd0a5f303
Artyom <admin@artnazarov.ru>
2016-05-07 12:51:19 +0500

    Update magnetsdb-python.py

--- magnetsdb-python.py
+++ magnetsdb-python.py
@@ -1,31 +1,72 @@
 #!/usr/bin/python
-
-CONST = {
-	"SEARCH_ACTION" :  "search",
-	"PREV_PAGE" : "prevpage",
-	"NEXT_PAGE" : "nextpage",
-	"REPEAT" : "repeat",
-	"QUIT" : "quit",
-	"PATH_TO_DB" : "F:\magnetsdb\main-sqlite.db",
-	"app"	: "qbittorrent.exe"
-	}
-print(CONST['SEARCH_ACTION'])
-print(CONST['PATH_TO_DB'])
-
 # import dependencies
 import sqlite3
+import os
 # require pip install terminaltables
 from terminaltables import AsciiTable
-# for calling external app
-import os
+from subprocess import call
 
+
+def output(args, table_data):
+	cls()
+	print('request: {} page:{}'.format(args['request'], args['page']))
+	table = AsciiTable(table_data)
+	print(table.table)
+
+def request(a):
+	conn = sqlite3.connect(CONST['PATH_TO_DB'])
+	sorting_field = 'caption'
+	order_by = 'asc'
+	offset = (int(a['page']) - 1) * 10
+	cursor = conn.execute(
+		"SELECT category, caption, labels, hash from data WHERE caption LIKE '%{}%' ORDER BY {} {} LIMIT 10 OFFSET {}".format(
+			a['request'], sorting_field, order_by, offset))
+	table_data = [['Num', 'Category', 'Caption', 'Labels', 'Hash']]
+	num = 0
+	for row in cursor:
+		# print("category={}".format(row[0]))
+		# print("caption={}".format(row[1]))
+		# print("labels={}".format(row[2]))
+		# print("hash={}".format(row[3]))
+		num = num + 1
+		table_data.append([str(num), row[0], row[1], row[2], row[3]])
+	return table_data
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def ext_app(C, td):
+	num = 99
+	while num > 0:
+		print("Download something? type 0 if not needed.")
+		num = int(input("Choice:"))
+		if num == 0:
+			break
+		call([
+			C['app'],
+			td[num][4]
+		])
 
 action_from_input = "start"
 request_from_input = "test"
 page_from_input = "1"
 
+
+CONST = {
+	"SEARCH_ACTION" :  "search",
+	"PREV_PAGE" : "prev",
+	"NEXT_PAGE" : "next",
+	"REPEAT" : "repeat",
+	"QUIT" : "quit",
+	"PATH_TO_DB" : "F:\magnetsdb\main-sqlite.db",
+	"app"	: "qbittorrent.exe"
+	}
+
+print(CONST['SEARCH_ACTION'])
+print(CONST['PATH_TO_DB'])
+
 while not action_from_input == CONST['QUIT']:
-	action_from_input = input("Action(available:search,repeat, nextpage,prevpage,quit): ")
+	action_from_input = input("Action(available:search,repeat,next,prev,quit): ")
 	if action_from_input == CONST["SEARCH_ACTION"]:
 		request_from_input = input("Request: ")
 		page_from_input = input("Page(1,2,etc): ")
@@ -44,34 +85,10 @@ while not action_from_input == CONST['QUIT']:
 	args = {'action': action_from_input, 'request': request_from_input, "page" : page_from_input}
 
 	if (args['action'] in ( CONST['REPEAT'], CONST['SEARCH_ACTION'],CONST['PREV_PAGE'], CONST['NEXT_PAGE'])):
-		conn = sqlite3.connect(CONST['PATH_TO_DB'])
-		sorting_field = 'caption'
-		order_by = 'asc'
-		offset = (int(args['page']) - 1 ) * 10
-		cursor = conn.execute("SELECT category, caption, labels, hash from data WHERE caption LIKE '%{}%' ORDER BY {} {} LIMIT 10 OFFSET {}".format(args['request'], sorting_field, order_by, offset))
-		table_data = [['Num', 'Category', 'Caption', 'Labels', 'Hash']]
-		num = 0
-		print('request: {} page:{}'.format(args['request'], args['page']))
-		for row in cursor:
-			#print("category={}".format(row[0]))
-			#print("caption={}".format(row[1]))
-			#print("labels={}".format(row[2]))
-			#print("hash={}".format(row[3]))
-			num = num + 1
-			table_data.append([str(num), row[0], row[1], row[2], row[3]])
-		table = AsciiTable(table_data)
-		print(table.table)		
-
-		num = 99
-		while num > 0:
-			print("Download something? type 0 if not needed.")
-			num = int(input("Choice:"))
-			if num == 0:
-				break
-			os.system("{} magnet:?xt=urn:btih:{}".format(
-					CONST['app'],
-					table_data[num][4]
-						))
+		if not args['action'] == CONST['REPEAT']:
+			table_data = request(args)
+		output(args, table_data)
+		ext_app(CONST, table_data)
 
 
 
