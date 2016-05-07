56ef3606fc8e282ce1d438b26ac61c4b5308806a
Artyom <admin@artnazarov.ru>
2016-05-07 12:20:38 +0500

    Update magnetsdb-python.py

--- magnetsdb-python.py
+++ magnetsdb-python.py
@@ -4,6 +4,7 @@ CONST = {
 	"SEARCH_ACTION" :  "search",
 	"PREV_PAGE" : "prevpage",
 	"NEXT_PAGE" : "nextpage",
+	"REPEAT" : "repeat",
 	"QUIT" : "quit",
 	"PATH_TO_DB" : "F:\magnetsdb\main-sqlite.db",
 	"app"	: "qbittorrent.exe"
@@ -12,20 +13,19 @@ print(CONST['SEARCH_ACTION'])
 print(CONST['PATH_TO_DB'])
 
 # import dependencies
-
 import sqlite3
 # require pip install terminaltables
 from terminaltables import AsciiTable
 # for calling external app
 import os
 
+
 action_from_input = "start"
+request_from_input = "test"
+page_from_input = "1"
+
 while not action_from_input == CONST['QUIT']:
-	action_from_input = input("Action(available:search,nextpage,prevpage): ")
-
-	request_from_input = "test"
-	page_from_input = "1"
-
+	action_from_input = input("Action(available:search,repeat, nextpage,prevpage,quit): ")
 	if action_from_input == CONST["SEARCH_ACTION"]:
 		request_from_input = input("Request: ")
 		page_from_input = input("Page(1,2,etc): ")
@@ -43,7 +43,7 @@ while not action_from_input == CONST['QUIT']:
 
 	args = {'action': action_from_input, 'request': request_from_input, "page" : page_from_input}
 
-	if (args['action'] in ( CONST['SEARCH_ACTION'],CONST['PREV_PAGE'], CONST['NEXT_PAGE'])):
+	if (args['action'] in ( CONST['REPEAT'], CONST['SEARCH_ACTION'],CONST['PREV_PAGE'], CONST['NEXT_PAGE'])):
 		conn = sqlite3.connect(CONST['PATH_TO_DB'])
 		sorting_field = 'caption'
 		order_by = 'asc'
@@ -51,6 +51,7 @@ while not action_from_input == CONST['QUIT']:
 		cursor = conn.execute("SELECT category, caption, labels, hash from data WHERE caption LIKE '%{}%' ORDER BY {} {} LIMIT 10 OFFSET {}".format(args['request'], sorting_field, order_by, offset))
 		table_data = [['Num', 'Category', 'Caption', 'Labels', 'Hash']]
 		num = 0
+		print('request: {} page:{}'.format(args['request'], args['page']))
 		for row in cursor:
 			#print("category={}".format(row[0]))
 			#print("caption={}".format(row[1]))
@@ -59,9 +60,7 @@ while not action_from_input == CONST['QUIT']:
 			num = num + 1
 			table_data.append([str(num), row[0], row[1], row[2], row[3]])
 		table = AsciiTable(table_data)
-		print(table.table)
-
-		print("next page")
+		print(table.table)		
 
 		num = 99
 		while num > 0:
